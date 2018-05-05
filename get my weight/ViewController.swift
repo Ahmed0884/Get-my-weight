b0ff3997ed2693c67e813eb6bf4becc9080687a7
Ahmed nabil <ahmednabil@Ahmeds-Mac.local>
2018-05-05 07:29:59 -0700

    Initial Commit

---
+++ get my weight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  get my weight
+//
+//  Created by Ahmed nabil on 5/5/18.
+//  Copyright © 2018 Ahmed nabil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
